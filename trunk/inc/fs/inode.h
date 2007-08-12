6d1e84dd84f2fb302766f6838283742f0f071dc0
brainix <brainix@b9563320-9421-0410-b604-179ae615efef>
2007-08-12 05:50:45 +0000

    upgraded to GPL 3.0

--- trunk/inc/fs/inode.h
+++ trunk/inc/fs/inode.h
@@ -6,9 +6,9 @@
 \*----------------------------------------------------------------------------*/
 
 /*
- | This program is Free Software; you can redistribute it and/or modify it under
+ | This program is free software: you can redistribute it and/or modify it under
  | the terms of the GNU General Public License as published by the Free Software
- | Foundation; either version 2 of the License, or (at your option) any later
+ | Foundation, either version 3 of the License, or (at your option) any later
  | version.
  |
  | This program is distributed in the hope that it will be useful, but WITHOUT
@@ -17,11 +17,7 @@
  | details.
  |
  | You should have received a copy of the GNU General Public License along with
- | this program; if not, write to:
- |
- |	The Free Software Foundation, Inc.
- |	59 Temple Place, Suite 330
- |	Boston MA 02111-1307
+ | this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _FS_INODE_H
